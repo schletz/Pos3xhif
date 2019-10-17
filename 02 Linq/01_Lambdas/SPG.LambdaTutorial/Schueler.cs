1e3664b0dcade61ae8027f5a6f2060f97db81ed3
schletz <schletz@gmail.com>
2019-10-17 07:47:10 +0200

    Commit 17/10/2019

---
+++ 02 Linq/01_Lambdas/SPG.LambdaTutorial/Schueler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPG.LambdaTutorial
+{
+    /// <summary>
+    /// Representiert ein Entity Schüler
+    /// </summary>
+    public class Schueler
+    {
+        /// <summary>
+        /// Gibt die eindeutige Nummer eines Schülers zurück oder legt diese fest.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gibt den Nachnamen eines Schülers zurück oder legt diesen fest.
+        /// </summary>
+        public string Nachame { get; set; }
+
+        /// <summary>
+        /// Gibt den Vornamen eines Schülers zurück, oder legt diesen fest.
+        /// </summary>
+        public string Vorname { get; set; }
+
+        /// <summary>
+        /// Gibt die Klasse eines  Schüler zurück, oder legt diese fest.
+        /// </summary>
+        public string Klasse { get; set; }
+    }
+}
---
+++ 02 Linq/01_Lambdas/SPG.LambdaTutorial/SchuelerList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPG.LambdaTutorial
+{
+
+    public class SchuelerList : List<Schueler>
+    { }
+}
