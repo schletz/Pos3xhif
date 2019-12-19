93a22603bd760965ef7cf10d15812eb5c6bb6e47
schletz <schletz@gmail.com>
2019-12-19 09:10:01 +0100

    Commit 19/12/2019

--- 02 Linq/05_Join_Grouping/Program.cs
+++ 02 Linq/05_Join_Grouping/Program.cs
@@ -21,7 +21,13 @@ namespace Grouping
             // *************************************************************************************
             // ÜBUNG 1: Wie viele Klassen gibt es pro Abteilung?
             // *************************************************************************************
-            var result1 = (object)null;
+            var result1 = from c in data.Schoolclass
+                          group c by c.C_Department into g
+                          select new
+                          {
+                              Department = g.Key,
+                              Count = g.Count()
+                          };
             Console.WriteLine("RESULT1");
             Console.WriteLine(JsonSerializer.Serialize(result1));
 
@@ -30,7 +36,14 @@ namespace Grouping
             //          Hinweis: Filtere mit Where nach dem Erstellen der Objekte mit Department
             //                   und Count
             // *************************************************************************************
-            var result2 = (object)null;
+            var result2 = from c in data.Schoolclass
+                          group c by c.C_Department into g
+                          where g.Count() > 10
+                          select new
+                          {
+                              Department = g.Key,
+                              Count = g.Count()
+                          };
             Console.WriteLine("RESULT2");
             Console.WriteLine(JsonSerializer.Serialize(result2));
 
@@ -38,7 +51,16 @@ namespace Grouping
             // ÜBUNG 3: Wann ist der letzte Test (Max von TE_Date) pro Lehrer und Fach der 5AHIF
             //          in der Tabelle Test?
             // *************************************************************************************
-            var result3 = (object)null;
+            var result3 = from t in data.Test
+                          where t.TE_Class == "5AHIF"
+                          group t by new { t.TE_Teacher, t.TE_Subject } into g
+                          select new
+                          {
+                              Teacher = g.Key.TE_Teacher,
+                              Subject = g.Key.TE_Subject,
+                              LastTest = g.Max(te => te.TE_Date)
+                          };
+
             Console.WriteLine("RESULT3");
             Console.WriteLine(JsonSerializer.Serialize(result3));
 
@@ -54,7 +76,21 @@ namespace Grouping
             // als Standardwert false geliefert werden. Achte außerdem auf die Rangfolge der Operatoren
             // ?? und &&.
             // *************************************************************************************
-            var result4 = (object)null;
+            var result4 = from l in data.Lesson
+                          where (l.L_Room?.StartsWith("C2") ?? false) && l.L_Day == 1 &&
+                                l.L_Hour == data.Lesson
+                                                .Where(le => le.L_Room == l.L_Room && le.L_Day == l.L_Day)
+                                                .Max(le => le.L_Hour)
+                          select new
+                          {
+                              Room = l.L_Room,
+                              Day = l.L_Day,
+                              Hour = l.L_Hour,
+                              Teacher = l.L_Teacher,
+                              Class = l.L_Class
+                          };
+
+            // {"Room":"C2.09","Day":1,"Hour":16,"Teacher":"PUC","Class":"5BBIF"}              
             Console.WriteLine("RESULT4");
             Console.WriteLine(JsonSerializer.Serialize(result4));
 
@@ -72,8 +108,26 @@ namespace Grouping
             //     join y in table2 on new { X1 = x.Field1, X2 = Field2 } equals new { X1 = y.Field1, X2 = y.Field2 }
             // Die Ausgabe muss natürlich dem Beispiel 4 entsprechen.
             // *************************************************************************************
-            var lastLesson = (object)null;
-            var result5 = (object)null;
+            var lastLesson = from l in data.Lesson
+                             group l by new { l.L_Room, l.L_Day } into g
+                             select new
+                             {
+                                 Room = g.Key.L_Room,
+                                 Day = g.Key.L_Day,
+                                 LastLesson = g.Max(le => le.L_Hour)
+                             };
+            var result5 = from l in data.Lesson
+                          join ll in lastLesson on new {X1 = l.L_Room, X2 = l.L_Day, X3 = l.L_Hour} equals 
+                                                   new {X1 = ll.Room,  X2 = ll.Day, X3 = ll.LastLesson}
+                          where (l.L_Room?.StartsWith("C2") ?? false) && l.L_Day == 1
+                          select new
+                          {
+                              Room = l.L_Room,
+                              Day = l.L_Day,
+                              Hour = l.L_Hour,
+                              Teacher = l.L_Teacher,
+                              Class = l.L_Class
+                          };
             Console.WriteLine("RESULT5");
             Console.WriteLine(JsonSerializer.Serialize(result5));
 
