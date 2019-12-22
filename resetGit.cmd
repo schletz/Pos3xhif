86239881211a51223f60c15aafd3bfd498ca79e7
Michael Schletz <schletz@gmail.com>
2019-12-22 07:43:41 +0100

    Commit 22/12/2019

--- resetGit.cmd
+++ resetGit.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-echo Achtung: Alle lokalen Žnderungen werden zurckgesetzt. Drcke CTRL+C zum Abbrechen.
+echo Achtung: Alle lokalen Änderungen werden zurückgesetzt. Drücke CTRL+C zum Abbrechen.
 pause
 git fetch --all
 git reset --hard origin/master
