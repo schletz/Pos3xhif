3cfb2aae997170d50fe40a997c6509f2d39fe2a7
Michael Schletz <schletz@gmail.com>
2020-01-19 09:47:48 +0100

    Commit 19/01/2020

---
+++ 04 WPF/11_MenusAndUserControls/ViewModels/EditPersonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControlApp.ViewModels
+{
+    /// <summary>
+    /// Viewmodel für das Control EditPerson.
+    /// </summary>
+    public class EditPersonViewModel
+    {
+        public string DemoText => "EditPerson Control";
+    }
+}
