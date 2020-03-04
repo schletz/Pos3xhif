2c3bcdae49852107f5dafb450459cbc1c481ced7
schletz <schletz@gmail.com>
2020-03-04 10:13:56 +0100

    Commit 20200304

--- 02 Linq/04_QuerySyntax/Program.cs
+++ 02 Linq/04_QuerySyntax/Program.cs
@@ -28,10 +28,7 @@ namespace QuerySyntax
             // *************************************************************************************
             // [{"TE_ID":450,"TE_Class":"4EHIF","TE_Teacher":"ZUM","TE_Subject":"BWM3","TE_Date":"2020-05-20T00:00:00","TE_Lesson":8},
             //  {"TE_ID":451,"TE_Class":"4EHIF","TE_Teacher":"ZUM","TE_Subject":"PRE","TE_Date":"2020-02-17T00:00:00","TE_Lesson":1}]
-            IEnumerable<Test> result1 = from t in data.Test
-                                        where t.TE_Class == "4EHIF" && t.TE_Teacher == "ZUM"
-                                        select t;
-
+            object result1 = null;
             Console.WriteLine("RESULT1");
             Console.WriteLine(JsonSerializer.Serialize(result1));
 
@@ -42,10 +39,7 @@ namespace QuerySyntax
             // [{"L_ID":27,"L_Untis_ID":16,"L_Class":"3BHIF","L_Teacher":"FZ","L_Subject":"DBI1","L_Room":"C2.10","L_Day":2,"L_Hour":1},
             //  {"L_ID":695,"L_Untis_ID":536,"L_Class":"3BHIF","L_Teacher":"FZ","L_Subject":"DBI1x","L_Room":"B3.07MM","L_Day":5,"L_Hour":5},
             //  {"L_ID":696,"L_Untis_ID":536,"L_Class":"3BHIF","L_Teacher":"FZ","L_Subject":"DBI1x","L_Room":"B3.07MM","L_Day":5,"L_Hour":6}]
-            IEnumerable<Lesson> result2 = from l in data.Lesson
-                                          where l.L_Teacher == "FZ" && l.L_Class == "3BHIF"
-                                          orderby l.L_Day, l.L_Hour
-                                          select l;
+            object result2 = null;
             Console.WriteLine(Environment.NewLine + "RESULT2");
             Console.WriteLine(JsonSerializer.Serialize(result2));
 
@@ -56,16 +50,7 @@ namespace QuerySyntax
             // [{"Subject":"DBI1","Room":"C2.10","Day":2,"Hour":1},
             //  {"Subject":"DBI1x","Room":"B3.07MM","Day":5,"Hour":5},
             //  {"Subject":"DBI1x","Room":"B3.07MM","Day":5,"Hour":6}]
-            var result3 = from l in data.Lesson
-                          where l.L_Teacher == "FZ" && l.L_Class == "3BHIF"
-                          orderby l.L_Day, l.L_Hour
-                          select new
-                          {
-                              Subject = l.L_Subject,
-                              Room = l.L_Room,
-                              Day = l.L_Day,
-                              Hour = l.L_Hour
-                          };
+            object result3 = null;
             Console.WriteLine(Environment.NewLine + "RESULT3");
             Console.WriteLine(JsonSerializer.Serialize(result3));
 
@@ -84,13 +69,7 @@ namespace QuerySyntax
             //  {"TeacherId":"PUH","TeacherName":"Puhm Ursula"},
             //  {"TeacherId":"WAA","TeacherName":"Wagner Anna"},
             //  {"TeacherId":"WK","TeacherName":"Wodnar Karl"}]
-            var result4 = from t in data.Teacher
-                          where t.Tests.Any(te => te.TE_Subject == "NW2")
-                          select new
-                          {
-                              TeacherId = t.T_ID,
-                              TeacherName = $"{t.T_Lastname} {t.T_Firstname}"
-                          };
+            object result4 = null;
             Console.WriteLine(Environment.NewLine + "RESULT4");
             Console.WriteLine(JsonSerializer.Serialize(result4));
 
@@ -102,9 +81,7 @@ namespace QuerySyntax
             //          die Ausgabe nach dem Fach.
             // *************************************************************************************
             // ["BWM1","BWM2","DBI1","E1","GAD","GGPB","NVS1y","POS1"]
-            IEnumerable<string> result5 = (from t in data.Test
-                                           where t.TE_Lesson > 7 && t.TE_ClassNavigation.C_ClassTeacher == "SZ"
-                                           select t.TE_Subject).Distinct();
+            object result5 = null;
             Console.WriteLine(Environment.NewLine + "RESULT5");
             Console.WriteLine(JsonSerializer.Serialize(result5));
 
@@ -114,15 +91,7 @@ namespace QuerySyntax
             // *************************************************************************************
             // [{"Class":"4AHIF","Teacher":"GAL","Subject":"E1"},
             //  {"Class":"4AHIF","Teacher":"NAI","Subject":"GGPB"}]
-            var result6 = from t in data.Test
-                          where t.TE_Date.DayOfWeek == DayOfWeek.Monday && 
-                                t.TE_ClassNavigation.C_ClassTeacher == "SZ"
-                          select new
-                          {
-                              Class = t.TE_Class,
-                              Teacher = t.TE_Teacher,
-                              Subject = t.TE_Subject
-                          };
+            object result6 = null;
             Console.WriteLine(Environment.NewLine + "RESULT6");
             Console.WriteLine(JsonSerializer.Serialize(result6));
 
@@ -135,11 +104,7 @@ namespace QuerySyntax
             // [{"TE_ID":510,"TE_Class":"5CHIF","TE_Teacher":"GC","TE_Subject":"BWM1","TE_Date":"2020-05-09T00:00:00","TE_Lesson":5},
             //  {"TE_ID":511,"TE_Class":"5CHIF","TE_Teacher":"GC","TE_Subject":"BWM2","TE_Date":"2020-06-12T00:00:00","TE_Lesson":9},
             //  {"TE_ID":512,"TE_Class":"5CHIF","TE_Teacher":"GC","TE_Subject":"BWM3","TE_Date":"2019-11-01T00:00:00","TE_Lesson":5}]
-            string kv = data.Schoolclass.SingleOrDefault(sc => sc.C_ID == "3AHIF").C_ClassTeacher;
-
-            IEnumerable<Test> result7 = from t in data.Test
-                                        where t.TE_Teacher == kv && t.TE_Class == "5CHIF"
-                                        select t;
+            object result7 = null;
             Console.WriteLine(Environment.NewLine + "RESULT7");
             Console.WriteLine(JsonSerializer.Serialize(result7));
 
@@ -156,16 +121,7 @@ namespace QuerySyntax
             //  {"Class":"2CHIF","Subject":"BSPKx","Teacher":"Csaszar","Time":"17:10"},
             //  {"Class":"2CHIF","Subject":"BSPKy","Teacher":"Hofbauer","Time":"17:10"},
             //  {"Class":"5AHIF","Subject":"POS1","Teacher":"Schletz","Time":"17:10"}]
-            var result8 = from l in data.Lesson
-                          where l.L_ClassNavigation.C_Department == "HIF" &&
-                                l.L_Day == 2 && l.L_HourNavigation.P_From.TimeOfDay > new TimeSpan(17, 0, 0)
-                          select new
-                          {
-                              Class = l.L_Class,
-                              Subject = l.L_Subject,
-                              Teacher = l.L_TeacherNavigation.T_Lastname,
-                              Time = l.L_HourNavigation.P_From.ToString("H:mm")
-                          };
+            object result8 = null;
             Console.WriteLine(Environment.NewLine + "RESULT8");
             Console.WriteLine(JsonSerializer.Serialize(result8));
 
@@ -175,9 +131,7 @@ namespace QuerySyntax
             //          ermittle dann mit Max() die späteste Stunde.
             // *************************************************************************************
             // 9
-            long result9 = (from t in data.Test
-                            where t.TE_Teacher == "PUH"
-                            select t.TE_Lesson).Max();
+            object result9 = null;
             Console.WriteLine(Environment.NewLine + "RESULT9");
             Console.WriteLine(JsonSerializer.Serialize(result9));
 
@@ -188,14 +142,7 @@ namespace QuerySyntax
             //           serialisiert wird.
             // *************************************************************************************
             // {"ID":"SZ","Lastname":"Schletz","Firstname":"Michael"}
-            var result10 = (from t in data.Teacher
-                           where t.T_ID == "SZ"
-                           select new
-                           {
-                               ID = t.T_ID,
-                               Lastname = t.T_Lastname,
-                               Firstname = t.T_Firstname
-                           }).SingleOrDefault();
+            object result10 = null;
             Console.WriteLine(Environment.NewLine + "RESULT10");
             Console.WriteLine(JsonSerializer.Serialize(result10));
 
