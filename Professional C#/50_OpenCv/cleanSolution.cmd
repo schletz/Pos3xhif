f63c3b418ba85c9633c7bad661e41ff2ac7873de
Michael Schletz <schletz@spengergasse.at>
2021-09-21 08:45:41 +0200

    Commit 20210921

--- Professional C#/50_OpenCv/cleanSolution.cmd
+++ Professional C#/50_OpenCv/cleanSolution.cmd
@@ -2,7 +2,8 @@ REM @echo off
 REM Löscht alle temporären Visual Studio Dateien
 rd /S /Q ".vs"
 rd /S /Q ".vscode"
-  
+rd /S /Q "packages"
+
 FOR /D /R %%d IN (*) DO (
   rd /S /Q "%%d\bin"
   rd /S /Q "%%d\obj"
