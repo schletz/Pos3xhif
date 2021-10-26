46486ba6686d3699130f791040dda08c164dce99
Michael Schletz <schletz@spengergasse.at>
2021-10-26 09:44:51 +0200

    Commit 20211026

---
+++ 01 Types/Uebungen/Anmeldesystem/RegistrationSystem/RegistrationSystem.Application/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RegistrationSystem.Application
+{
+    public class Program
+    {
+        private static int _testCount = 0;
+        private static int _testsSucceeded = 0;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Klassenimplementierung.");
+            CheckAndWrite(() => typeof(Applicant).GetConstructor(Type.EmptyTypes) is null, "Kein Defaultkonstruktor in Applicant.");
+            CheckAndWrite(() => typeof(AcceptedApplicant).GetConstructor(Type.EmptyTypes) is null, "Kein Defaultkonstruktor in AcceptedApplicant.");
+
+            CheckAndWrite(
+                () => typeof(RegistrationService).GetProperties().Any() && typeof(RegistrationService).GetProperties().All(p => p.CanWrite == false),
+                "Alle Properties in RegistrationService sind read only.");
+            CheckAndWrite(
+                () => typeof(Applicant).GetProperties().Any() && typeof(Applicant).GetProperties().All(p => p.CanWrite == false),
+                "Alle Properties in Applicant sind read only.");
+            CheckAndWrite(
+                () => typeof(AcceptedApplicant).GetProperties().Any() && typeof(AcceptedApplicant).GetProperties().All(p => p.CanWrite == false),
+                "Alle Properties in AcceptedApplicant sind read only.");
+            CheckAndWrite(() => typeof(IGradeChecker).IsInterface, "IGradeChecker ist ein Interface.");
+            CheckAndWrite(() => typeof(HtlGradeChecker).IsAssignableTo(typeof(IGradeChecker)), "HtlGradeChecker implementiert IGradeChecker.");
+            CheckAndWrite(() => typeof(FsGradeChecker).IsAssignableTo(typeof(IGradeChecker)), "FsGradeChecker implementiert IGradeChecker.");
+
+            CheckAndWrite(
+                () => typeof(Applicant).GetProperty(nameof(Applicant.Grades))?.PropertyType == typeof(IReadOnlyDictionary<string, int>),
+                "Applicant.Grades ist IReadOnlyDictionary<string, int>.");
+
+            Console.WriteLine("Teste GradeCheckers");
+            var htlGradeChecker = new HtlGradeChecker();
+            CheckAndWrite(() =>
+                htlGradeChecker.CanBeAccepted(new Dictionary<string, int> { { "D", 1 }, { "E", 2 }, { "M", 3 } })
+                && !htlGradeChecker.CanBeAccepted(new Dictionary<string, int> { { "D", 1 }, { "E", 2 }, { "M", 4 } }), "HtlGradeChecker.CanBeAccepted rechnet richtig.");
+            var fsGradeChecker = new FsGradeChecker();
+            CheckAndWrite(() =>
+                fsGradeChecker.CanBeAccepted(new Dictionary<string, int> { { "D", 1 }, { "E", 2 }, { "M", 4 } })
+                && !fsGradeChecker.CanBeAccepted(new Dictionary<string, int> { { "D", 1 }, { "E", 2 }, { "M", 5 } }), "FsGradeChecker.CanBeAccepted rechnet richtig.");
+
+            Console.WriteLine("Teste Applicants");
+            var applicantHif = new Applicant(firstname: "A", lastname: "B", email: "C", department: "HIF");
+            applicantHif.AddGrade("D", 1); applicantHif.AddGrade("E", 2); applicantHif.AddGrade("M", 3);
+            CheckAndWrite(() => applicantHif.Grades.Count == 3 && applicantHif.Grades["D"] == 1, "AddGrades fügt die Noten hinzu.");
+            CheckAndWrite(() => !applicantHif.AddGrade("D", 2) && applicantHif.Grades.Count == 3 && applicantHif.Grades["D"] == 1, "AddGrades lehnt vorhandene Noten ab.");
+            var acceptedApplicant = new AcceptedApplicant(applicantHif);
+            CheckAndWrite(() => acceptedApplicant.Email == applicantHif.Email && acceptedApplicant.DateAccepted > DateTime.UtcNow.AddMinutes(-1), "Konstruktor von AcceptedApplicant setzt die Werte.");
+
+            Console.WriteLine("Teste RegistrationService");
+            var applicantHifToBad = new Applicant(firstname: "A", lastname: "B", email: "D", department: "HIF");
+            applicantHifToBad.AddGrade("D", 1); applicantHifToBad.AddGrade("E", 2); applicantHifToBad.AddGrade("M", 4);
+
+            var applicantduplicateMail = new Applicant(firstname: "A", lastname: "B", email: "C", department: "HIF");
+            applicantduplicateMail.AddGrade("D", 1); applicantduplicateMail.AddGrade("E", 2); applicantduplicateMail.AddGrade("M", 3);
+
+            var applicantFs = new Applicant(firstname: "A", lastname: "B", email: "E", department: "FIT");
+            applicantFs.AddGrade("D", 1); applicantFs.AddGrade("E", 2); applicantFs.AddGrade("M", 4);
+
+            var applicantFsToBad = new Applicant(firstname: "A", lastname: "B", email: "F", department: "FIT");
+            applicantFsToBad.AddGrade("D", 1); applicantFsToBad.AddGrade("E", 2); applicantFsToBad.AddGrade("M", 5);
+
+            var applicantNoGradeCheckDepartment = new Applicant(firstname: "A", lastname: "B", email: "G", department: "HBGM");
+            applicantNoGradeCheckDepartment.AddGrade("D", 5); applicantNoGradeCheckDepartment.AddGrade("E", 5); applicantNoGradeCheckDepartment.AddGrade("M", 5);
+
+            var registrationService = new RegistrationService();
+            registrationService.AddApplicant(applicantHif);
+            registrationService.AddApplicant(applicantHifToBad);
+            registrationService.AddApplicant(applicantFs);
+            registrationService.AddApplicant(applicantFsToBad);
+            registrationService.AddApplicant(applicantNoGradeCheckDepartment);
+
+            CheckAndWrite(() => registrationService.Applicants.Count == 5, "RegistrationService.Applicants zeigt die richtige Anzahl an Bewerbern.");
+            CheckAndWrite(() => !registrationService.AddApplicant(applicantduplicateMail), "RegistrationService.AddApplicant prüft Emails auf Eindeutigkeit.");
+            registrationService.RegisterGradeChecker("HIF", htlGradeChecker);
+            registrationService.RegisterGradeChecker("FIT", fsGradeChecker);
+
+            CheckAndWrite(() => registrationService.AcceptApplicant(applicantHif.Email), "RegistrationService.AcceptApplicant akzeptiert Bewerber mit ausreichenden Noten.");
+            CheckAndWrite(() => !registrationService.AcceptApplicant("X"), "RegistrationService.AcceptApplicant liefert false, wenn die Email nicht existiert.");
+            CheckAndWrite(() => !registrationService.AcceptApplicant(applicantHif.Email), "RegistrationService.AcceptApplicant liefert false, wenn der Bewerber schon akzeptiert wurde.");
+            CheckAndWrite(() => !registrationService.AcceptApplicant(applicantHifToBad.Email), "RegistrationService.AcceptApplicant liefert false, wenn die Noten für die HTL nicht passen.");
+            CheckAndWrite(() =>
+                registrationService.AcceptApplicant(applicantFs.Email)
+                && !registrationService.AcceptApplicant(applicantFsToBad.Email), "RegistrationService.AcceptApplicant liefert false, wenn die Noten für die FS nicht passen.");
+            CheckAndWrite(() =>
+                registrationService.AcceptApplicant(applicantNoGradeCheckDepartment.Email),
+                "RegistrationService.AcceptApplicant berpcksichtigt keine Noten, wenn kein GradeChecker für die Abteilung definiert wurde.");
+            Console.WriteLine($"{_testsSucceeded} von {_testCount} Punkte erreicht.");
+        }
+
+        private static void CheckAndWrite(Func<bool> predicate, string message)
+        {
+            _testCount++;
+            if (predicate())
+            {
+                Console.WriteLine($"   {_testCount} OK: {message}");
+                _testsSucceeded++;
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"   {_testCount} Nicht erfüllt: {message}");
+            Console.ResetColor();
+        }
+    }
+}
