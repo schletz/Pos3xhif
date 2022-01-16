01fdf6cd2b25d577634883b176f6387158f4fed5
Michael Schletz <schletz@spengergasse.at>
2022-01-16 17:44:46 +0100

    Commit 20220116

---
+++ md2html.py
@@ -0,0 +1,80 @@
+# REQUIREMENTS
+# pip install markdown
+# pip install pygments
+
+import markdown as md
+import sys, re, requests, base64
+
+# **************************************************************************************************
+# Codiert eine Datei und gibt einen Base64 String zurück, der direkt als img src verwendet werden kann.
+def encodeFile(filename):
+    extension = re.search("^.+\.(?P<ext>.*)$", filename).group("ext")
+    with open(filename, "rb") as file:
+        mimeType = "image/svg+xml" if extension == "svg" else f'image/{extension}'
+        content = file.read()
+        contentString = base64.b64encode(content).decode("utf-8")
+        return f'data:{mimeType};charset=utf-8;base64,{contentString}'
+# **************************************************************************************************
+
+if len(sys.argv) < 2:
+    raise TypeError("Usage: python md2html.py filename")
+
+filename = re.search("^(?P<name>.*)\.md$", sys.argv[1])
+if filename is None:
+    raise TypeError("Usage: python md2html.py filename")
+
+mdFilename = filename.group()
+htmlFilename = f'{filename.group("name")}.html'
+
+# CSS aus dem Template lesen
+req = requests.get("https://raw.githubusercontent.com/microsoft/vscode/main/extensions/markdown-language-features/media/markdown.css")
+cssString = req.text
+
+with open(mdFilename, "r", encoding="utf-8") as input_file:
+    mdString = input_file.read()
+
+htmlString = md.markdown(mdString,
+    output_format="html5",
+    encoding='utf8',
+    extensions=['tables', 'codehilite', 'fenced_code'],
+    extension_configs={
+        'fenced_code': {
+        },
+        'codehilite': {
+            'linenums': False,
+            'noclasses': True
+        }
+    }
+)
+
+# Bilder durch Base64 Content ersetzen.
+images = list(map(lambda x: (
+    {
+        'element': x.group(),
+        'file': x.group('filename'),
+        'content': encodeFile(x.group('filename'))
+    }), re.finditer("\<img [^\>]*src=\"(?P<filename>[^\"]+)\"[^\>]*\>", htmlString)))
+
+for image in images:
+    htmlString = htmlString.replace(image["element"], f'<img src=\"{image["content"]}\">')
+
+with open(htmlFilename, "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+    output_file.write(f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{mdFilename}</title>
+    <style type="text/css">
+        {cssString}
+    </style>
+</head>    
+<body>
+    """)
+    output_file.write(htmlString)
+    output_file.write("""
+</body>
+</html>    
+    """)
