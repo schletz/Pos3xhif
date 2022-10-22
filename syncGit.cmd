00d8d4c04ab8b03bdc98a2ad7627784275a90fda
schletz <schletz@spengergasse.at>
2022-10-22 09:33:16 +0200

    Commit 22/10/2022

--- syncGit.cmd
+++ syncGit.cmd
@@ -1,7 +1,15 @@
+@echo off
 REM https://stackoverflow.com/questions/203090/how-do-i-get-current-datetime-on-the-windows-command-line-in-a-suitable-format
-REM mit Adaptierungen fï¿½r das deutsche Datumsformat (geht nur bei deutschem Windows!)
+REM mit Adaptierungen für das deutsche Datumsformat (geht nur bei deutschem Windows!)
 For /f "tokens=1-3 delims=. " %%a in ('date /t') do (set mydate=%%c%%b%%a)
 git add -A
 git commit -a -m "Commit %mydate%"
 git pull
+IF ERRORLEVEL 1 GOTO error
 git push
+IF ERRORLEVEL 1 GOTO error
+GOTO end
+:error
+echo Ein Fehler ist aufgetreten.
+pause
+:end
