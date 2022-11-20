04dcdc1d1add1a627bb04df74b3b2f69c2fd099d
schletz <schletz@spengergasse.at>
2022-11-20 21:03:19 +0100

    Commit 20221120

--- HolidayCalendar/CalendarService.cs
+++ HolidayCalendar/CalendarService.cs
@@ -10,7 +10,11 @@ namespace HolidayCalendar;
 class CalendarService
 {
     private readonly CalendarDay[] _calendarDays;
-    private readonly DateTime _day0;
+    private readonly int _yearFrom;
+    private readonly int _yearToExclusive;
+    private readonly DateTime _minDate;
+    private readonly DateTime _maxDate;
+    private readonly ArgumentException _outOfRangeException;
     /// <summary>
     /// Erzeugt ein durchgehendes Array mit allen Tagen der angegebenen Range.
     /// </summary>
@@ -18,8 +22,9 @@ class CalendarService
     {
         if (yearFrom >= yearToExclusive) { throw new ArgumentException("Invalid year range."); }
 
-        _day0 = new DateTime(yearFrom, 1, 1);
-        int daysCount = (int)((new DateTime(yearToExclusive, 1, 1).Ticks - _day0.Ticks) / TimeSpan.TicksPerDay);
+        _minDate = new DateTime(yearFrom, 1, 1);
+        _maxDate = new DateTime(yearToExclusive, 1, 1);
+        int daysCount = (int)((new DateTime(yearToExclusive, 1, 1).Ticks - _minDate.Ticks) / TimeSpan.TicksPerDay);
         var days = new CalendarDay[daysCount];
         var daysSpan = days.AsSpan();
         int i = 0;
@@ -28,38 +33,38 @@ class CalendarService
             var calendarYear = new CalendarYear(year);
             i += calendarYear.GetCalendarDays(daysSpan.Slice(i, DaysOfYear(year)));
         }
+        _yearFrom = yearFrom;
+        _yearToExclusive = yearToExclusive;
         _calendarDays = days;
+        _outOfRangeException = new ArgumentException($"Date is not in Range. Only years {_yearFrom} to {_yearToExclusive - 1} are allowed.");
     }
 
-    public IReadOnlyList<CalendarDay> CalendarDays => _calendarDays;
+    public DateTime MinDate => _minDate;
+    public DateTime MaxDate => _maxDate;
+    public int YearFrom => _yearFrom;
+    public int YearToExclusive => _yearToExclusive;
+    public CalendarDay[] CalendarDays => _calendarDays;
+
     public int DaysOfYear(int year) => 365 + (year % 400 == 0 ? 1 : year % 100 == 0 ? 0 : year % 4 == 0 ? 1 : 0);
     /// <summary>
-    /// Gibt die Position des Tages im internen Array zurück.
+    /// Liefert die Kalendertage eines Jahres.
     /// </summary>
-    public int CalcDayNumber(DateTime date) => (int)((date.Date.Ticks - _day0.Ticks) / TimeSpan.TicksPerDay);
-    /// <summary>
-    /// Liefert einen Enumerator, der alle Tage eines Jahres aufzählt.
-    /// </summary>
-    public IEnumerable<CalendarDay> GetDaysOfYear(int year)
+    public CalendarDay[] GetDaysOfYear(int year)
     {
-        var i = CalcDayNumber(new DateTime(year, 1, 1));
-        var last = i + DaysOfYear(year);
-        while (i < last)
-        {
-            yield return _calendarDays[i++];
-        }
+        var start = new DateTime(year, 1, 1);
+        var first = CalcDayNumber(start);
+        var last = CalcDayNumber(start.AddYears(1).AddDays(-1));
+        return _calendarDays[first..(last + 1)];
     }
     /// <summary>
-    /// Liefert einen Enumerator, der alle Tage eines Monats aufzählt.
+    /// Liefert die Kalendertage eines Monats.
     /// </summary>
-    public IEnumerable<CalendarDay> GetDaysOfMonth(int year, int month)
+    public CalendarDay[] GetDaysOfMonth(int year, int month)
     {
-        var i = CalcDayNumber(new DateTime(year, month, 1));
-        var last = CalcDayNumber(new DateTime(year, month + 1, 1));
-        while (i < last)
-        {
-            yield return _calendarDays[i++];
-        }
+        var start = new DateTime(year, month, 1);
+        var first = CalcDayNumber(start);
+        var last = CalcDayNumber(start.AddMonths(1).AddDays(-1));
+        return _calendarDays[first..(last + 1)];
     }
     /// <summary>
     /// Bestimmt die Anzahl der Arbeitstage zwischen 2 Datumswerten.
@@ -80,9 +85,19 @@ class CalendarService
         while (_calendarDays[i].WorkingdayCounter != counter)
         {
             i += workingDays < 0 ? -1 : 1;
+            if (i < 0 || i >= _calendarDays.Length) { throw _outOfRangeException; }
         }
         if (workingDays < 0) i++; // Der erste Tag der Serie mit gleichen Counter Werten.
         return _calendarDays[i].DateTime;
+    }
+    /// <summary>
+    /// Gibt die Position des Tages im internen Array zurück.
+    /// </summary>
+    private int CalcDayNumber(DateTime date)
+    {
+        int i = (int)((date.Date.Ticks - _minDate.Ticks) / TimeSpan.TicksPerDay);
+        if (i < 0 || i >= _calendarDays.Length) { throw _outOfRangeException; }
+        return i;
     }
 
 }
--- HolidayCalendar/CalendarServiceTests.cs
+++ HolidayCalendar/CalendarServiceTests.cs
@@ -10,14 +10,14 @@ public class CalendarServiceTests
     {
         var service = new CalendarService(2000, 2400);
         // 2100, 2200 und 2300 sind keine Schaltjahre nach dem gregorianischem Kalender. Daher -3.
-        Assert.True(service.CalendarDays.Count == 400 * 365 + 100 - 3);
+        Assert.True(service.CalendarDays.Length == 400 * 365 + 100 - 3);
     }
     [Fact]
     public void CalendarDaysAscendingTest()
     {
         var days = new CalendarService(2000, 2400).CalendarDays;
         var start = new DateTime(2000, 1, 1);
-        for (int i = 0; i < days.Count; i++)
+        for (int i = 0; i < days.Length; i++)
         {
             if (days[i].DateTime != start.AddDays(i))
             {
@@ -30,23 +30,28 @@ public class CalendarServiceTests
     [Fact]
     public void GetDaysOfYearTest()
     {
-        var service = new CalendarService(2000, 2400);
+        var service = new CalendarService(2000, 2101);
         var dayOfYear = service.GetDaysOfYear(2100);
-        Assert.True(dayOfYear.First().DateTime == new DateTime(2100, 1, 1) && dayOfYear.Last().DateTime == new DateTime(2100, 12, 31));
+        Assert.True(dayOfYear.Length == 365 && dayOfYear[0].DateTime == new DateTime(2100, 1, 1) && dayOfYear[364].DateTime == new DateTime(2100, 12, 31));
     }
     [Fact]
     public void GetDaysOfMonthTest()
     {
-        var service = new CalendarService(2000, 2400);
-        var dayOfYear = service.GetDaysOfMonth(2000, 2);
-        Assert.True(dayOfYear.First().DateTime == new DateTime(2000, 2, 1) && dayOfYear.Last().DateTime == new DateTime(2000, 2, 29));
+        var service = new CalendarService(2022, 2026);
+        var days1 = service.GetDaysOfMonth(2025, 12);
+        var days2 = service.GetDaysOfMonth(2024, 2);
+        Assert.True(days1.Length == 31 && days1[0].DateTime == new DateTime(2025, 12, 1) && days1[30].DateTime == new DateTime(2025, 12, 31));
+        Assert.True(days2.Length == 29 && days2[0].DateTime == new DateTime(2024, 2, 1) && days2[28].DateTime == new DateTime(2024, 2, 29));
+        Assert.Throws<ArgumentException>(() => service.GetDaysOfMonth(2021, 12));
+        Assert.Throws<ArgumentException>(() => service.GetDaysOfMonth(2026, 1));
+
     }
     [Fact]
     public void CalendarDaysWorkingCounterTest()
     {
         var days = new CalendarService(2000, 2400).CalendarDays;
         int workingDayCounter = 0;
-        foreach(var day in days)
+        foreach (var day in days)
         {
             if (day.IsWorkingDayMoFr) { workingDayCounter++; }
             if (day.WorkingdayCounter != workingDayCounter)
