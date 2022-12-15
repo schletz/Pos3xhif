f078fe003366c3ca29aad1e9ff57250ff4e67b80
schletz <schletz@spengergasse.at>
2022-12-15 09:56:10 +0100

    Commit 20221215

--- resetGit.cmd
+++ resetGit.cmd
@@ -3,6 +3,8 @@ chcp 65001
 echo Achtung: Alle lokalen Änderungen werden zurückgesetzt. Drücke CTRL+C zum Abbrechen.
 pause
 
+FOR /F "tokens=*" %%a IN ('git branch --show-current') DO (SET current_branch=%%a)
+
 git fetch --all --prune
 FOR /F "tokens=* delims=* " %%a IN ('git branch') DO (
     echo Reset branch %%a...
@@ -10,3 +12,5 @@ FOR /F "tokens=* delims=* " %%a IN ('git branch') DO (
     git clean -df
     git reset --hard origin/%%a
 )
+git checkout %current_branch%
+echo You are in branch %current_branch%
--- resetGit.sh
+++ resetGit.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Setzt alle lokalen Branches zurück
 
+current_branch=$(git branch --show-current)
 for branch in $(git branch | tr '*' ' ')
 do
     echo Reset branch $branch
-    git checkout %%a
+    git checkout $branch
     git clean -df
     git reset --hard origin/$branch
 done
+
+git checkout $current_branch
+echo "You are in Branch $current_branch"
