c73624a9995f4809b64520f5da1b0e5c3387453f
Michael Schletz <schletz@spengergasse.at>
2026-04-24 19:39:40 +0200

    Qwen3.6 container

--- 07 Asciidoctor/local_llm/start_vllm_gemma_31b.cmd
+++ 07 Asciidoctor/local_llm/start_vllm_gemma_31b.cmd
@@ -1,15 +1,17 @@
-docker build -t my-vllm-gemma4 .
+docker build -t my-vllm .
 docker create --gpus all ^
   -v %USERPROFILE%/.cache/huggingface:/root/.cache/huggingface ^
   -p 8000:8000 ^
-  --name vllm-gemma4 ^
-  my-vllm-gemma4 ^
-  --model LilaRest/gemma-4-31B-it-NVFP4-turbo ^
-  --quantization modelopt ^
-  --max-model-len 16384 ^
-  --max-num-seqs 128 ^
-  --max-num-batched-tokens 16384 ^
+  --name vllm-qwen3 ^
+  my-vllm ^
+  RedHatAI/Qwen3.6-35B-A3B-NVFP4 ^
+  --reasoning-parser qwen3 ^
+  --moe_backend flashinfer_cutlass ^
+  --max-model-len 65536 ^
+  --max-num-seqs 1 ^
+  --max-num-batched-tokens 32768 ^
   --gpu-memory-utilization 0.9 ^
   --kv-cache-dtype fp8 ^
   --enable-prefix-caching ^
-  --trust-remote-code
+  --trust-remote-code ^
+  --limit-mm-per-prompt "{\"image\": 0, \"video\": 0, \"audio\": 0}"
